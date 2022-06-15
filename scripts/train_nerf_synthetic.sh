91a2d7708060197fbdb8d558a9fb2168650de606
eric <cgx1997@outlook.com>
2022-06-15 14:50:12 -0700

    added script to train on nerf_synthetic

---
+++ scripts/train_nerf_synthetic.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# define shape names
+declare -a scene_names=(
+    "chair"
+    "drums"
+    "ficus"
+    "hotdog"
+    "lego"
+    "materials"
+    "mic"
+    "ship")
+
+for scene_name in ${scene_names[@]}; do
+    python main_nerf.py data/nerf_synthetic/$scene_name \
+    --workspace workspace/$scene_name \
+    --mode blender \
+    --bound 1.0 \
+    --scale 0.8 \
+    --dt_gamma 0 \
+    --color_space linear
+done
